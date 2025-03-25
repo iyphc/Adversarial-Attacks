57d768bf2ce745ddc728d518d6560235ba32e8ec
iyphc <werhistudy@yandex.ru>
2025-03-25 23:11:40 +0300

    moved evaluation to evaluation.py

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from data import get_dataloaders
 from model import LittleCNN
 from tqdm import tqdm
+from evaluation import evaluate_model
 
 def get_device():
     if torch.cuda.is_available():
@@ -12,20 +13,6 @@ def get_device():
         return torch.device("mps")
     else:
         return torch.device("cpu")
-
-def evaluate_model(model, test_loader, device):
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    accuracy = 100 * correct / total
-    return accuracy
 
 def train_model(epochs=10, learning_rate=0.001, batch_size=128, device='cuda'):
     device = get_device()

b822cc833fd5a124ee84922e02268d429ec675fa
iyphc <werhistudy@yandex.ru>
2025-03-26 17:53:57 +0300

    added creating subset function

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import torch
+from torch.utils.data import DataLoader, Subset
+
+def create_subset_dataloader(dataloader, num_samples, batch_size=128):
+    indices = list(range(num_samples))
+    subset = Subset(dataloader.dataset, indices)
+    return DataLoader(subset, batch_size=batch_size, shuffle=False)
 
 def get_device():
     if torch.cuda.is_available():
